762b8a61dd93df08b0626e6c09434c7a4b73c42c
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-03-22 04:22:55 +0100

    Create unique.js

---
+++ unique.js
@@ -0,0 +1,11 @@
+// Function to generate a unique identifier (UUID)
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+// Generate and display a unique identifier
+console.log("Unique Identifier (UUID):", generateUUID());
