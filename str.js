a7237fd83f088390120338daba605a9d21a71304
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-03-27 05:14:56 +0200

    Create str.js

---
+++ str.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+const originalString = 'Hello, world!';
+const reversedString = reverseString(originalString);
+console.log(reversedString); // Output: "!dlrow ,olleH"
