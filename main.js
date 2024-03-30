e88d00ca01161acfa9e2a9840fd2ee6df7c6981d
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-03-30 04:24:52 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+public class StringArrayExample {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Orange", "Mango"};
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
