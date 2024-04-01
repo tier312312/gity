7840a7d301e320db3d1bf32d32284fd5648f43bc
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-04-01 05:07:13 +0200

    Create print.js

---
+++ print/print.js
@@ -0,0 +1,8 @@
+public class AverageCalculator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        double average = (double) (num1 + num2) / 2;
+        System.out.println("Average: " + average);
+    }
+}
