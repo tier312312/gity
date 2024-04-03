c867dcf9836e3a3fe57f48efe626b222b57504da
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-04-03 05:00:55 +0200

    Create Date.js

---
+++ Date.js
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class DateExample {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Current date and time: " + currentDate);
+    }
+}
