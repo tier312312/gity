4d469d2e5ac8468b7e945ac0dba45964f73221d6
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-04-08 04:59:41 +0200

    Create area.js

---
+++ area.js
@@ -0,0 +1,7 @@
+public class CircleArea {
+    public static void main(String[] args) {
+        double radius = 5.0;
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of circle: " + area);
+    }
+}
