23d41d1da4f50e185724b2968940c557fcee88dc
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-04-12 08:25:23 +0200

    Create indexfg.js

---
+++ indexfg.js
@@ -0,0 +1,7 @@
+public class StringLastIndexOf {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int index = str.lastIndexOf('o');
+        System.out.println("Last index of 'o': " + index);
+    }
+}
