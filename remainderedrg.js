dda6ae564257b1ead30661279487a8da4bc6817a
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-04-14 05:47:29 +0200

    Create remainderedrg.js

---
+++ remainderedrg.js
@@ -0,0 +1,17 @@
+public class PalindromeNumberCheck {
+    public static void main(String[] args) {
+        int number = 12321;
+        int originalNumber = number;
+        int reversedNumber = 0;
+        while (number != 0) {
+            int remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number /= 10;
+        }
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " is a palindrome.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome.");
+        }
+    }
+}
