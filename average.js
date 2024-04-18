e5078341b92a30465492d3c5883226304fbf0f8f
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-04-18 05:26:30 +0200

    Create average.js

---
+++ average.js
@@ -0,0 +1,7 @@
+public class AverageCalculation {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+        double average = (num1 + num2) / 2.0;
+        System.out.println("Average: " + average);
+    }
+}
