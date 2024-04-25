3d5361642f5a72d724c2c1b4e61dd4467ade3613
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-04-25 05:10:03 +0200

    Create replacedStrsdfsd.js

---
+++ replacedStrsdfsd.js
@@ -0,0 +1,7 @@
+public class StringReplace {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String replacedStr = str.replace('o', 'X');
+        System.out.println("Replaced string: " + replacedStr);
+    }
+}
