525ad89b8a9ff35adbd494d2eb97083a67c1295c
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-04-26 03:33:45 +0200

    Create substringadfsds.js

---
+++ substringadfsds.js
@@ -0,0 +1,7 @@
+public class SubstringExample {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String substr = str.substring(7);
+        System.out.println("Substring: " + substr);
+    }
+}
