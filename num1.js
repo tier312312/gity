28f167a2e7fa36c22931b2e1aaf3042d3c8a6ab0
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-04-27 05:00:32 +0200

    Create num1.js

---
+++ num1.js
@@ -0,0 +1,10 @@
+public class GCD {
+    public static void main(String[] args) {
+        int num1 = 12, num2 = 18, gcd = 1;
+        for (int i = 1; i <= num1 && i <= num2; ++i) {
+            if (num1 % i == 0 && num2 % i == 0)
+                gcd = i;
+        }
+        System.out.println("" + num1 + " и " + num2 + " = " + gcd);
+    }
+}
