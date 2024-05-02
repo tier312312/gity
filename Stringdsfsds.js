bef4a6f274cd56a9f16d9f8f9da15d2578c66b54
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-05-02 05:30:44 +0200

    Create Stringdsfsds.js

---
+++ Stringdsfsds.js
@@ -0,0 +1,10 @@
+public class IfElseExample {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num > 0) {
+            System.out.println("Number is positive.");
+        } else {
+            System.out.println("Number is non-positive.");
+        }
+    }
+}
