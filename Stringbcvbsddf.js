342eb941ea3c9e913b4e65e75eb7273aff8451b1
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-05-03 06:05:35 +0200

    Create Stringbcvbsddf.js

---
+++ Stringbcvbsddf.js
@@ -0,0 +1,10 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
