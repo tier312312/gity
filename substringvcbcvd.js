a3463f920d1c6eca0d7c93bc5699757bc8bd1971
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-05-05 06:12:39 +0200

    Create substringvcbcvd.js

---
+++ substringvcbcvd.js
@@ -0,0 +1,7 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        System.out.println("Length of the string: " + str.length());
+        System.out.println("Substring: " + str.substring(7));
+    }
+}
