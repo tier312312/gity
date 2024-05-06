18b8ed1c1338df82fcdfe1f6ab9028e02f29b11b
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-05-06 04:47:35 +0200

    Create Stringsdfsdwe.js

---
+++ Stringsdfsdwe.js
@@ -0,0 +1,7 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; ++i) {
+            System.out.println("Value of i: " + i);
+        }
+    }
+}
