38af21ac578defb8c4cf25d47bfb9789d1e24e50
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-05-08 02:22:31 +0200

    Create Positiveq3re23r.js

---
+++ Positiveq3re23r.js
@@ -0,0 +1,10 @@
+public class IfElseExample {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x > 0) {
+            System.out.println("Positive number.");
+        } else {
+            System.out.println("Non-positive number.");
+        }
+    }
+}
