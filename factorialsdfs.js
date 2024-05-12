e580849c9988e076e5e27b49750bdc355b8810d3
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-05-12 05:10:57 +0200

    Create factorialsdfs.js

---
+++ factorialsdfs.js
@@ -0,0 +1,12 @@
+public class FactorialCalculator {
+    public static int factorial(int n) {
+        if (n == 0)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is " + factorial(num));
+    }
+}
