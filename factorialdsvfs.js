c4d1832d2dec4809067b453dce4fb60b65616634
tier312312 <134521680+tier312312@users.noreply.github.com>
2024-05-20 04:14:01 +0200

    Create factorialdsvfs.js

---
+++ factorialdsvfs.js
@@ -0,0 +1,13 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Factorial of " + n + " is " + factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+}
